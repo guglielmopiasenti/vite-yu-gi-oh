5b7467e95398e5a9e5149406e7ee77f9a04b4060
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-03 16:56:35 +0200

    writing pokemonlist

---
+++ src/components/pokemons/PokemonList.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+export default {
+    data() {
+        return {
+            pokemon: []
+
+        }
+    },
+    created() {
+        axios.get(endpoint).then(res => {
+            this.pokemon = res.data.docs;
+        });
+    }
+};
+</script>
+<template>
+    <section id="pokemon"></section>
+</template>
+<style></style>
