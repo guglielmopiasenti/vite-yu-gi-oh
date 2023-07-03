777c78fe07a5955ccfe29c9cf3db4830609ea533
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-03 21:28:53 +0200

    corrected path

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import axios from 'axios';
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
 import AppMain from './components/AppMain.vue';
-import { store } from 'src/assets/data/store.js';
+import { store } from './assets/data/store.js';
 export default {
     components: { AppMain },
 
