d8a0fb855dcb7adab5c6b8d00261f3393e265f6c
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-03 16:56:18 +0200

    writing appmain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,12 @@
+<script>
+import PokemonList from './pokemons/PokemonList.vue';
+export default {
+    components: { PokemonList }
+};
+</script>
+<template>
+    <main>
+        <PokemonList />
+    </main>
+</template>
+<style></style>
