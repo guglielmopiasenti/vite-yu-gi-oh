d2019260338c8c0023058bb32af9c663865c3daf
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-04 17:38:20 +0200

    importing searchform to app

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@ import axios from 'axios';
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
 import AppMain from './components/AppMain.vue';
 import { store } from './assets/data/store.js';
+import SearchForm from './components/pokemons/SearchForm.vue';
 export default {
-    components: { AppMain },
+    components: { AppMain, SearchForm },
 
     created() {
         axios.get(endpoint).then(res => {
