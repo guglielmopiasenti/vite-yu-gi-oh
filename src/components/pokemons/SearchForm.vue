d235a2254871dacaffc804f82596e97f9e8f8c90
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-06 19:59:04 +0200

    adding options form

--- src/components/pokemons/SearchForm.vue
+++ src/components/pokemons/SearchForm.vue
@@ -1,11 +1,21 @@
 <script>
 export default {
-
+    data() {
+        return {
+            selectedOption: ''
+        }
+    },
+    props: {
+        options: Array,
+        defaultLabel: String,
+    },
+    emits: ['type-change']
 }
 </script>
 
 <template>
-    <select class="form-select">
-        <option selected> All types </option>
+    <select class="form-select mb-3" v-model="selectedOption" @change="$emit('option-change', selected option)">
+        <option selected> {{ defaultLabel || '---' }} </option>
+        <option v-for="option in options" :key="option"> {{ option }}</option>
     </select>
 </template>
